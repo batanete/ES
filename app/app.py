2f1b862bc4829c63a8f4fe485f107f58c840d465
HugoSilveiro <hugofilipe95@hotmail.com>
2017-04-18 22:03:18 +0100

    playlists render

--- app/app.py
+++ app/app.py
@@ -326,5 +326,5 @@ if __name__=='__main__':
     app = connexion.App(__name__, specification_dir='swagger/')
     app.add_api('swagger.yaml')
 
-    app.app.secret_key('cenas lixadas')
+    #app.app.secret_key('cenas lixadas')
     app.run(port=8000)
