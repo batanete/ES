4cafaeb2d81a4d79e9143465461760cb2da7f3f5
batanete <bata619@gmail.com>
2017-04-22 06:23:17 +0100

    added fibonacci example method for aws

--- app/application.py
+++ app/application.py
@@ -14,6 +14,19 @@ def make_session_permanent():
     session.permanent = True
     application.permanent_session_lifetime = timedelta(minutes=1)
 
+
+def fibonacci(n):
+    if n <= 2:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+@application.route('/computefibonacci')
+def httpfibonacci():
+    try:
+        n = int(request.args['n'])
+    except:
+        return 'Wrong arugments', 404
+    return 'Fibonacci(' + str(n) + ') = ' + str(fibonacci(n))
+
 if __name__=='__main__':
     application.secret_key='cenas lixadas'
     app.run(host='127.0.0.1',port=8000)
